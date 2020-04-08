010dd5a30d9dc1938c33fdb6432ecd9a014841a5
mari <mari.17ymy@gmail.com>
2020-04-08 19:08:31 -0400

    cambiando de pantallas y trabajando con columnas

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:primera_app/src/primera_clase.dart';
 
 void main(){
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return new Center(
-     child: new Directionality(
-       textDirection: TextDirection.ltr,
-       child: new Text('holaaaaaaaaaaaaaaaaaa')
-     )
-   );
-  }
-
-}
 
