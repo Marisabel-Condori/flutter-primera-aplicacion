93ab0f2eb253dd420a996e673c6259fdd4266eea
mari <mari.17ymy@gmail.com>
2020-04-09 00:14:09 -0400

    adicionando boton flotante

--- lib/src/pages/home_pages.dart
+++ lib/src/pages/home_pages.dart
@@ -1,25 +1,36 @@
 import 'package:flutter/material.dart';
 
-
-
-class HomePage extends StatelessWidget{
-
+class HomePage extends StatelessWidget {
   final tamanio = new TextStyle(fontSize: 25);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Titulo'),
-      ),
-      body: Center(child: Column(children: <Widget>[
-        Text('numero de clicks', style: tamanio,),
-        Text('0', style: tamanio,)
-      ],
-      mainAxisAlignment: MainAxisAlignment.center,
-      ),) 
-    );
+        appBar: AppBar(
+          title: Text('Titulo'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text(
+                'numero de clicks',
+                style: tamanio,
+              ),
+              Text(
+                '0',
+                style: tamanio,
+              )
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: (){
+            print('hola mundo');
+          }
+          ),
+        );
   }
-
-
 }
+
