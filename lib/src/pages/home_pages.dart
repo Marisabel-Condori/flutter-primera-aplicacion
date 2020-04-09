0259d9adbbf5760abd4f9784507165b17bcf5950
mari <mari.17ymy@gmail.com>
2020-04-09 00:53:05 -0400

    cambiar color al appBar y la posicion del boton flotante

--- lib/src/pages/home_pages.dart
+++ lib/src/pages/home_pages.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,7 +8,10 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Titulo'),
+          title: Text('Titulo', 
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.orangeAccent,
         ),
         body: Center(
           child: Column(
